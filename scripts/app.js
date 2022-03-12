3e85e991a58c12442b9b7d932d575f876dbbf4f0
Toby Austin Locke <tobyaustinlocke@gmail.com>
2022-03-12 20:59:18 +0000

    changes to cv modal

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,17 @@
-let ejs = require("ejs");
+// Objects
 
 const portfolioNavLinkElement = document.getElementById("portfolio-nav-link");
 const navPortfolioDropdownElement = document.getElementById("nav-dropdown");
 const navPortfolioDropdownList = document.getElementById("nav-dropdown-list");
+
+const cvBtn = document.getElementById("about-text").children[2].firstChild;
+
+const modalExitIcon = document.getElementsByClassName("modal-exit")[0];
+const modalBackgroundElement =
+  document.getElementsByClassName("modal-background")[0];
+const modalElement = document.getElementsByClassName("modal")[0];
+
+// Event Listeners
 
 portfolioNavLinkElement.addEventListener("mouseover", showNavPortfolioDropdown);
 
@@ -10,26 +19,28 @@ navPortfolioDropdownList.addEventListener(
   "mouseover",
   showNavPortfolioDropdown
 );
-
 navPortfolioDropdownElement.addEventListener(
   "mouseover",
   showNavPortfolioDropdown
 );
-
 portfolioNavLinkElement.addEventListener(
   "mouseleave",
   hideNavPortfolioDropdown
 );
-
 navPortfolioDropdownElement.addEventListener(
   "mouseleave",
   hideNavPortfolioDropdown
 );
-
 navPortfolioDropdownList.addEventListener(
   "mouseleave",
   hideNavPortfolioDropdown
 );
+
+cvBtn.addEventListener("click", openModal);
+
+modalExitIcon.addEventListener("click", closeModal);
+
+// Functions
 
 function showNavPortfolioDropdown(event) {
   var mouseHover = true;
@@ -45,4 +56,17 @@ function hideNavPortfolioDropdown(event) {
   if (mouseHover === true) {
     showNavPortfolioDropdown();
   }
+}
+
+function openModal() {
+  modalBackgroundElement.classList.remove("modal-hide");
+  modalElement.classList.remove("modal-hide");
+  modalExitIcon.classList.remove("modal-hide");
+  console.log("clicked");
+}
+
+function closeModal() {
+  modalBackgroundElement.classList.add("modal-hide");
+  modalElement.classList.add("modal-hide");
+  modalExitIcon.classList.add("modal-hide");
 }
